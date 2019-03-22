dfd9430688f9bc2da472b9a30da971640c65e0df
Colin Smith <colinsmith@MacBook-Pro-6.local>
2019-03-22 12:10:04 -0600

    first commit

---
+++ Model/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  MovieSearch
+//
+//  Created by Colin Smith on 3/22/19.
+//  Copyright © 2019 Colin Smith. All rights reserved.
+//
+
+import Foundation
---
+++ ModelController/MovieController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieController.swift
+//  MovieSearch
+//
+//  Created by Colin Smith on 3/22/19.
+//  Copyright © 2019 Colin Smith. All rights reserved.
+//
+
+import Foundation
+
+
+
+class MovieController {
+    
+    
+}
